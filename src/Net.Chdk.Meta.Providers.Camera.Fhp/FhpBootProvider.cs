7b7bac8bdaf7d414904a18cbc219975367a225bd
Dmitry Shechtman <damnian@gmail.com>
2017-07-09 06:36:48 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.Camera.Fhp/FhpBootProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Net.Chdk.Meta.Providers.Camera.Fhp
+{
+    sealed class FhpBootProvider : BootProvider
+    {
+        protected override string GetBootFileSystem(uint modelId) => "FAT32";
+    }
+}
---
+++ src/Net.Chdk.Meta.Providers.Camera.Fhp/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Net.Chdk.Meta.Providers.Camera.Eos;
+
+namespace Net.Chdk.Meta.Providers.Camera.Fhp
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFhpProviders(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<IBootProvider, FhpBootProvider>()
+                .AddSingleton<IEosCardProvider, FhpCardProvider>();
+        }
+    }
+}
